2313fc55f6dc8d4cedcd7b9a89a06f41fda491fb
Mike Endale <mike@blencorp.com>
2020-05-20 06:11:41 -0400

    Updates

---
+++ precache-manifest.243b3b7890878ee442d76c5da4fe6779.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6170bd432f1c86f594d62c261276b9e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27d39c635c783c128caf",
+    "url": "/static/css/main.65804060.chunk.css"
+  },
+  {
+    "revision": "86ec5b2ed2e156ba3c70",
+    "url": "/static/js/2.a79170a5.chunk.js"
+  },
+  {
+    "revision": "f3559a585326a5bcf7c913f6e55005fd",
+    "url": "/static/js/2.a79170a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d39c635c783c128caf",
+    "url": "/static/js/main.ef485260.chunk.js"
+  },
+  {
+    "revision": "2ef077c869eae91f19e9",
+    "url": "/static/js/runtime-main.808624db.js"
+  },
+  {
+    "revision": "88c16ac388ede1f19c91268091bfcf3b",
+    "url": "/static/media/ECRT_Horizontal_Logo.88c16ac3.svg"
+  },
+  {
+    "revision": "7fd272b0661e2bd8ec86e9361e9b6e01",
+    "url": "/static/media/EITLogo.7fd272b0.png"
+  },
+  {
+    "revision": "aee89369088d66b7d0cc46ca15fb9fd4",
+    "url": "/static/media/ENELEGNA.aee89369.JPG"
+  },
+  {
+    "revision": "a2faa9ecf3975bc6ccaa8b4e451f4e76",
+    "url": "/static/media/acr_toolkit_logo.a2faa9ec.webp"
+  },
+  {
+    "revision": "d5360b11dc4ecede11365c8b68f63a91",
+    "url": "/static/media/africa_covid19_2.d5360b11.svg"
+  },
+  {
+    "revision": "322e5a60d60cd553d1d3d033ce0e266e",
+    "url": "/static/media/arrow_drop_down-24px.322e5a60.svg"
+  },
+  {
+    "revision": "a82b3f01b2b6c791b9eaf144060c34c9",
+    "url": "/static/media/arrow_right-24px.a82b3f01.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "21b753d9765d4a3743a4c5f59813e0fc",
+    "url": "/static/media/news_3.21b753d9.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  }
+]);
